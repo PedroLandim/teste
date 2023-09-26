b4cc0b393cd01784454204c3e12a30c49a000919
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2023-09-26 20:14:48 -0300

    Add files via upload

---
+++ scripts/canvas.js
@@ -0,0 +1,176 @@
+class Canvas {
+    constructor(id) {
+        this.canvas = document.getElementById(id);
+        this.canvasCtx = this.canvas.getContext('2d');
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        this.canvas.addEventListener("mousedown", (event) => {
+            isDragging = true;
+            currentPointIndex = -1;
+            
+            const newPoint = updateCurrentPoint(event);
+            const index = verifyIntersection(newPoint);
+            if (index > -1) {
+                currentPointIndex = index;
+            } else {
+                addPointToCurve();
+            }
+            this.redraw();
+        });
+        
+        this.canvas.addEventListener("mousemove", (event) => {
+            if (!isDragging) {
+                return;
+            }
+            currentPoint = updateCurrentPoint(event);
+            editCurvePoint(currentPoint, currentPointIndex);
+            this.redraw();
+        });
+        
+        this.canvas.addEventListener("mouseup", (event) => {
+            isDragging = false;
+        });
+    }
+
+    clear() {
+        const { width, height } = this.canvas;
+        this.canvasCtx.clearRect(0, 0, width, height);
+    }
+
+    drawPoint(point, color = "#CCC") {
+        this.canvasCtx.beginPath();
+        let radius = pointRadius;
+        if (isCurrentPointIndex(point)) {
+            radius *= 2;
+        }
+        this.canvasCtx.fillStyle = color;
+        this.canvasCtx.arc(point.x, point.y, radius , 0, 2 * Math.PI);
+        this.canvasCtx.fill();
+    }
+    
+    drawLine(pointA, pointB, lineColor) {
+        this.canvasCtx.beginPath();
+        this.canvasCtx.moveTo(pointA.x, pointA.y);
+        this.canvasCtx.lineTo(pointB.x, pointB.y);
+        this.canvasCtx.strokeStyle = lineColor;
+        this.canvasCtx.lineWidth = 1;
+        this.canvasCtx.stroke();
+    }
+
+    drawLines(points, color, isControl = false) {
+        for (let index = 1; index < points.length; index++) {
+            const point = points[index];
+            if (pontosInput.checked) {
+                const pointColor = isControl ?  "#CCC" : "red";
+                this.drawPoint(point, pointColor);
+            }
+            if ((linhasInput.checked && isControl) || !isControl) {
+                this.drawLine(points[index - 1], point, color);   
+            }
+        }
+    }
+
+    interpolate(pointA, pointB, t) {
+        return {
+            x: ((1-t) * pointA.x + t * pointB.x),
+            y: ((1-t) * pointA.y + t * pointB.y)
+        }
+    }
+
+    deCasteljau(points, t, canReplay = false) {
+        const grade = points.length - 1;
+        if (grade === 1) {
+            if (canReplay) replayCurves.push({
+                A: points[0],
+                B: points[1], 
+                type: "line" 
+            });
+            return this.interpolate(points[0], points[1], t);
+        } 
+        const newPoints = [];
+        for (let i = 0; i < grade; i++) {
+            const pointA = points[i];
+            const pointB = points[i + 1];
+            const newPoint = this.interpolate(pointA, pointB, t)
+            if (canReplay) {
+                replayCurves.push({
+                    A: pointA,
+                    B: pointB, 
+                    type: "line" 
+                });
+                replayCurves.push({
+                    A: pointA, 
+                    B: pointB, 
+                    point: newPoint, 
+                    type: "interpolate" 
+                });
+            }
+            newPoints.push(newPoint);
+        }
+        return this.deCasteljau(newPoints, t, canReplay);
+    }
+
+    drawBezierCurve(curve, isCurrentCurve = false) {
+        const points = curve.points;
+        if (points.length <= 2) {
+            return;
+        }
+
+        let bezierCurves = [];
+        if (isCurrentCurve) {
+            replayCurves = [{
+                type: "control", curve
+            }];
+        }
+        bezierCurves.push(points[0]);
+        for (let i = 1; i <= numIterations - 2; i++) {
+            const position = i / numIterations;
+            const newPoint = this.deCasteljau(points, position, isCurrentCurve);
+            if (isCurrentCurve) replayCurves.push({ 
+                point: newPoint, 
+                type: "point", 
+                position 
+            });
+            bezierCurves.push(newPoint);
+        }
+        bezierCurves.push(points[points.length - 1]);
+        if (isCurrentCurve) {
+            replayCurves.push({
+                type: "bezier", bezierCurves
+            });
+            replayInput.max = replayCurves.length;
+        }
+        this.drawLines(bezierCurves, isCurrentCurve ? "#ff6600" : "#000");
+    }
+
+    
+    drawControlPoligonals(curves) {
+        curves.forEach(curve => {
+            if (curve.points.length === 0) {
+                return;
+            }
+            if (pontosInput.checked) {
+                this.drawPoint(curve.points[0]);
+            }
+            if (curve.points.length === 1) {
+                return;
+            }
+            this.drawLines(curve.points, curve.color, true);
+        });
+    }
+
+    redraw() {
+        this.clear();
+        if (curvasInput.checked) {
+            for (let index = 0; index < curves.length; index++) {
+                const curve = curves[index];
+                this.drawBezierCurve(curve, index === currentCurve);
+            }
+        }
+        this.drawControlPoligonals(curves);
+    }
+}
+
+const canvas = new Canvas('canvas');
+const replay = new Canvas('replay');
---
+++ scripts/globals.js
@@ -0,0 +1,118 @@
+var curves = [{
+    points: [],
+    color: '#CCC'
+}];
+var currentPoint = {
+    x: 0, y: 0
+};
+var replayCurves = [];
+
+var pointRadius = 3;
+var isDragging = false;
+
+var currentCurve = 0;
+var numIterations = 5;
+var currentPointIndex = -1;
+
+var iterationsInput = null;
+var replayContainer = null;
+var linhasInput = null;
+var pontosInput = null;
+var curvasInput = null;
+var replayInput = null;
+
+function loadDomReferences() {
+    iterationsInput = document.querySelector("#avaliacoes");
+    linhasInput = document.querySelector("input[name='linhas']");
+    pontosInput = document.querySelector("input[name='pontos']");
+    curvasInput = document.querySelector("input[name='curvas']");
+    replayInput = document.querySelector(".slider");
+}
+
+function intersectPoint(pointA, pointB) {
+    const XPoint = pointA.x - pointB.x;
+    const YPoint = pointA.y - pointB.y;
+    const difference = Math.pow(XPoint, 2) + Math.pow(YPoint, 2);
+    return Math.sqrt(difference) <= pointRadius;
+}
+
+function isCurrentPointIndex(point) {
+    if (currentPointIndex === -1) return false;
+    const currentPoint = curves[currentCurve].points[currentPointIndex];
+    if (intersectPoint(point, currentPoint)) {
+        return true;
+    }
+    return false;
+}
+
+function verifyIntersection(point) {
+    const currentPoints = curves[currentCurve].points;
+    for (let index = 0; index < currentPoints.length; index++) {
+        const element = currentPoints[index];
+        if (intersectPoint(point, element)) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+function updateCurrentPoint(event) {
+    currentPoint = {
+        x: event.offsetX,
+        y: event.offsetY
+    };
+    return currentPoint;
+}
+
+function editCurvePoint(point, index = -1) {
+    if (index === -1) {
+        index = curves[currentCurve].points.length - 1;
+    }
+    curves[currentCurve].points[index] = point;
+}
+
+function addPointToCurve() {
+    curves[currentCurve].points.push(currentPoint);
+}
+
+function addNewCurve() {
+    curves.push({
+        points: [],
+        color: '#CCC'
+    });
+    currentCurve = curves.length - 1;
+}
+
+function redraw() {
+    canvas.redraw();
+}
+
+function switchCurrentCurve() {
+    currentCurve = (currentCurve + 1) % curves.length;
+    redraw()
+}
+
+function deleteCurve() {
+    curves.splice(currentCurve, 1);
+    if (curves.length === 0) {
+        addNewCurve();
+    }
+    currentCurve = (currentCurve + 1) % curves.length;
+    redraw();
+}
+
+function deletePoint() {
+    if (currentPointIndex === -1) {
+        curves[currentCurve].points.pop();
+    } else {
+        curves[currentCurve].points.splice(currentPointIndex, 1);
+        currentPointIndex -= 1;
+    }
+    redraw();
+}
+
+function updateNumIterations(event) {
+    numIterations = event.value;
+    iterationsInput.value = numIterations;
+    redraw();
+}
